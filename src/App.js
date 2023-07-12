aeaf9d842c1dd59b64eb153095672b045c90317b
Meaga J <119093848+jmeaga@users.noreply.github.com>
2023-07-12 15:46:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+// import { BrowserRouter,Routes,Route } from 'react-router-dom';
+// import Dashbaord from './Dashboard';
+// import Login from './Login';
+// import Signup from './Signup';
+//import About from './About';
+//import Contact from './Contact';
+import Main from './Main';
+//import { Domain } from '@mui/icons-material';
+function App() {
+  return (
+//      <BrowserRouter>
+// <Routes>
+// <Route path="/" element={<Signup/>}></Route>
+//  <Route path="/Signup" element={<Signup/>}></Route>
+//  <Route path="/Login" element={<Login/>}></Route>
+//  <Route path="/Home" element={<Home/>}></Route>
+//  <Route path="/About" element={<About/>}</Route>
+//  </Routes>
+//  </BrowserRouter>
+    //<Login/>
+    //<div><Dashboard/></div>
+    // <div><About/></div>
+       //<div><Contact/></div>
+       <div><Main/></div>
+  );
+}
+
+export default App;
